30c76a5c6ffe5447314e553a6e1d100370881709
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-01-23 22:33:56 +0530

    init✌️

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <ProductCard name="Audio Setup" price="3250/="
+      description="HIi svbbh hfhfgdv hfhhkkgiun kgyhgn"
+      img="https://www.shutterstock.com/image-photo/loei-thailand-september-12-2023-260nw-2360603941.jpg"/>
+     
+      <ProductCard name="Head-set" price="1523/=" description="Introducing JBL T450BT on-ear wireless headphones."
+      img="https://m.media-amazon.com/images/I/61kFL7ywsZS._AC_UF1000,1000_QL80_.jpg"/>
+    </>
+  )
+}
+
+export default App
