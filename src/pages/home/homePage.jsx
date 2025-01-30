2b8beceb6454ebe654c8134a4ba386dc66e13a97
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-01-31 00:45:33 +0530

    day 11 new changes ☑️

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,9 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
+import Home from "./home";
+import Contact from "../contact";
+import Gallery from "../gallery";
+import Items from "../items";
+import ErrorNotFound from "../error";
 
 export default function HomePage() {
     return (
-        <div className="w-full h-screen flex justify-center ">
-            <Header/>
-        </div>
+       <>
+       <Header/>
+       <div className="h-[calc(100vx-100px)] w-full">
+         <Routes path="/*">
+           <Route path="/contact" element={<Contact/>}/>
+           <Route path="/gallery" element={<Gallery/>}/>
+           <Route path="/items" element={<Items/>}/>
+           <Route path="/" element={<Home/>}/>
+           <Route path="/*" element={<ErrorNotFound/>}/>
+           
+         
+         
+         
+         </Routes>
+
+       </div>
+       
+       </>
     );
 }
