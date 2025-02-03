e5d3cc82393a8b6522582686e5b9042e6cf02c1e
Imasha Sandanayaka <imashasandanayaka@gmail.com>
2025-02-03 21:53:06 +0530

    testing ✌️

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css"
 import AdminPage from "./pages/admin/adminComponent";
 import HomePage from "./pages/home/homePage";
+import Testing from "./components/testing";
+import LoginPage from "./pages/login/login";
 function App()  {
   return (
   <BrowserRouter>
     <Routes path="/*">
+       <Route path="/testing" element={<Testing/>}/>
+       <Route path="/login" element={<LoginPage/>}/>
        <Route path="/admin/*" element={<AdminPage/>}/>
        <Route path="/" element={<HomePage/>}/>
        <Route path="/*" element={<HomePage/>}/>
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export default function LoginPage(){
+  const [email,setEmail]=useState("");
+  cconst [PiPassword,setPassword]=useState("");
+  
+  return ( 
+   <div className="w-full h-screen flex justify-center items-center">
+       
+    </div>)
+}
